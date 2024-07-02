c2d4d2065d529f9a180dbc2a19feb6c01ff7f4d5
Choi-syoon <neulet.git@gmail.com>
2024-07-03 01:20:29 +0900

    StatefulWidget 적용

--- app_gemini/lib/main.dart
+++ app_gemini/lib/main.dart
@@ -1,37 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gemini/flutter_gemini.dart';  // Flutter imports
 
 void main() {
-  runApp(const MyApp());
+  runApp(Application());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+class Application extends StatefulWidget{
   @override
-  Widget build(BuildContext context) {
+  _ApplicationState createState() => _ApplicationState();
+}
+
+class _ApplicationState extends State<Application>{
+  @override
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Gemini App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Gemini App'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
+      home: Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: Text("Gemini"),
       ),
       body: Center(
         child: Column(),
@@ -47,6 +32,7 @@ class MyHomePage extends StatelessWidget {
             labelText: "Prompt"
           ),
         ),
+      )
       )
     );
   }

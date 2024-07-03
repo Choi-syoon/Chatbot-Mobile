fdf294d1b4daaa763f648d81e13e7bdbe51319a4
Choi-syoon <neulet.git@gmail.c0om>
2024-07-03 17:51:54 +0900

    send button 구현

--- app_gemini/lib/main.dart
+++ app_gemini/lib/main.dart
@@ -4,13 +4,12 @@ void main() {
   runApp(Application());
 }
 
-class Application extends StatefulWidget{
+class Application extends StatefulWidget {
   @override
   _ApplicationState createState() => _ApplicationState();
 }
 
-class _ApplicationState extends State<Application>{
-
+class _ApplicationState extends State<Application> {
   final _promptTextEditController = TextEditingController();
 
   @override
@@ -20,33 +19,46 @@ class _ApplicationState extends State<Application>{
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("Gemini"),
-      ),
-      body: Center(
-        child: Column(),
-      ),
-      bottomSheet: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: TextField(
-          controller: _promptTextEditController,
-          keyboardType: TextInputType.text,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20.0),
-            ),
-            labelText: "Prompt",
-          ),
-          onChanged: (text) {
-            var _promptTextArray = text;
-          },
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text("Gemini"),
         ),
-      )
-      )
+        body: Center(
+          child: Column(),
+        ),
+        bottomSheet: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: _promptTextEditController,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    labelText: "Prompt",
+                  ),
+                  onChanged: (text) {},
+                ),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {
+                  // Handle send button press
+                  String promptText = _promptTextEditController.text;
+                  print('Send button pressed with text: $promptText');
+                },
+                child: Text("Send"),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
